347b65eaee0fdedd98a0ca38c1d9c306877913ec
RhysArmour <rhysarmour@hotmail.com>
2023-03-17 16:46:45 +0000

    adjusted db files

--- src/utils/database/guild-db.js
+++ src/utils/database/guild-db.js
@@ -37,9 +37,8 @@ const checkRoomsAreAssigned = async (serverId) => {
 
 const assignStrikes = async (user, serverId) => {
   const duplicate = await guildDb.findOne({
-    where: { Name: user.username, UniqueId: user.id, ServerId: serverId },
+    where: {  Name: user.username, UniqueId: user.id, ServerId: serverId  },
   });
-  console.log('DUPLICATE', duplicate)
   if (!duplicate) {
     console.log('CREATING ENTRY')
     return guildDb.create({
@@ -55,10 +54,9 @@ const assignStrikes = async (user, serverId) => {
 };
 
 const updateStrike = async (user, serverId) => {
-  console.log('UPDATE STRIKES FOR', user)
-  await guildDb.increment('Strikes', { by: 1, where: { Name: user.username, UniqueId: user.id, ServerId: serverId } });
-  await guildDb.increment('TotalStrikes', { by: 1, where: { Name: user.username, UniqueId: user.id, ServerId: serverId } });
-  return guildDb.findOne({ where: { Name: user.username, UniqueId: user.id, ServerId: serverId } });
+  await guildDb.increment('Strikes', { by: 1, where: { Name: user.username, UniqueId: user.id, ServerId: serverId }});
+  await guildDb.increment('TotalStrikes', { by: 1, where: { Name: user.username, UniqueId: user.id, ServerId: serverId }});
+  return guildDb.findOne({ where: {Name: user.username, UniqueId: user.id, ServerId: serverId}});
 };
 
 const getStrikes = async (user, serverId) => {
